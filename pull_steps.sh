f46c130ce6da6371b92197dfd29ab18fa28ffc3b
douglasdcm <douglas.dcm@gmail.com>
2017-10-24 18:52:53 -0200

    commit from sh file

---
+++ pull_steps.sh
@@ -0,0 +1,6 @@
+cd /home/douglas/dcs/gcs/biblioteca_impressos
+git pull https://github.com/douglasdcm/douglasdcm.github.io.git
+git add --all
+git status
+git commit -m "commit from sh file"
+git push https://github.com/douglasdcm/douglasdcm.github.io.git
