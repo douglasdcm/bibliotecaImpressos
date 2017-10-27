621082849e59d644ed10485bcc8aadad1bab193a
douglasdcm <douglas.dcm@gmail.com>
2017-10-27 08:45:52 -0200

    teste

--- pull_steps.sh
+++ pull_steps.sh
@@ -2,8 +2,8 @@ cd /home/douglas/dcs/gcs/biblioteca_impressos
 git pull https://github.com/douglasdcm/douglasdcm.github.io.git
 git add --all
 git status
-echo Enter commit message 
+echo Enter commit message: 
 read m
-git commit -m "commit from sh file" $m
+git commit -m $m
 git push https://github.com/douglasdcm/douglasdcm.github.io.git
 git status
